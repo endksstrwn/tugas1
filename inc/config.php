d062a762da40f36972476482b65174b97dbb3578
endksstrwn <khairulmuhamadrizkiml@gmail.com>
2025-12-04 20:46:44 +0800

    penambahan pw db

--- inc/config.php
+++ inc/config.php
@@ -7,12 +7,12 @@ if (session_status() === PHP_SESSION_NONE) {
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// Sesuaikan port dengan server Anda (misal 8000 atau 8080)
+
 const BASE_URL = 'http://localhost:8000/';
 
 const DB_HOST = 'localhost';
 const DB_USER = 'root';
-const DB_PASS = ''; // Sesuaikan password database Anda
+const DB_PASS = 'komangendik35'; 
 const DB_NAME = 'toko_buku_db';
 
 spl_autoload_register(function ($class_name) {
